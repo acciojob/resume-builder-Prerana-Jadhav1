dfe94b76be58596887e6845ba00d4bbc7c6a5742
Prerana-Jadhav1 <preranajb5@gmail.com>
2026-09-09 12:43:33 +0530

    Create actions.js

---
+++ src/redux/actions.js
@@ -0,0 +1,23 @@
+export const SET_PROFILE = "SET_PROFILE";
+export const ADD_EDUCATION = "ADD_EDUCATION";
+export const DELETE_EDUCATION = "DELETE_EDUCATION";
+export const ADD_SKILL = "ADD_SKILL";
+export const DELETE_SKILL = "DELETE_SKILL";
+export const ADD_PROJECT = "ADD_PROJECT";
+export const DELETE_PROJECT = "DELETE_PROJECT";
+export const ADD_SOCIAL = "ADD_SOCIAL";
+export const DELETE_SOCIAL = "DELETE_SOCIAL";
+export const SET_STEP = "SET_STEP";
+export const RESET_RESUME = "RESET_RESUME";
+
+export const setProfile = (profile) => ({ type: SET_PROFILE, payload: profile });
+export const addEducation = (education) => ({ type: ADD_EDUCATION, payload: education });
+export const deleteEducation = (index) => ({ type: DELETE_EDUCATION, payload: index });
+export const addSkill = (skill) => ({ type: ADD_SKILL, payload: skill });
+export const deleteSkill = (index) => ({ type: DELETE_SKILL, payload: index });
+export const addProject = (project) => ({ type: ADD_PROJECT, payload: project });
+export const deleteProject = (index) => ({ type: DELETE_PROJECT, payload: index });
+export const addSocial = (social) => ({ type: ADD_SOCIAL, payload: social });
+export const deleteSocial = (index) => ({ type: DELETE_SOCIAL, payload: index });
+export const setStep = (step) => ({ type: SET_STEP, payload: step });
+export const resetResume = () => ({ type: RESET_RESUME });
